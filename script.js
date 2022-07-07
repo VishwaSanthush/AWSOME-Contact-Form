28824e5d046b647d01e3ab859cce1acbcac471f6
Santhush Dharmawardhana <99556165+VishwaSanthush@users.noreply.github.com>
2022-07-07 14:38:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+//Get data
+
+const nameInput = document.querySelector("name");
+const email = document.querySelector("email");
+const message = document.querySelector("message");
+const success = document.querySelector("success");
+const errorNodes = document.querySelector(".error");
+
+//Validate data
+function validateForm(){
+
+   clearMessage();
+   let errorFlag = false;
+
+   if(nameInput.ariaValueMax.length < 1){
+      errorNodes[0].innerText = "Name Cannot be blank";
+      nameInput.classList.add("error-border");
+      errorFlag = true;
+   }
+
+   if(!emailIsValid(email.value)){
+      errorNodes[1].innerText = "Invalid email address";
+      email.classList.add("error-border");
+      errorFlag = true;
+   }
+
+   if(message.value.length < 1){
+      errorNodes[2].innerText = "Please enter message";
+      message.classList.add("error-border"); 
+      errorFlag = true;
+   }
+
+   if(!errorFlag){
+      success.innerText = "Success!"
+   }
+}
+
+//Clear error / success message
+function clearMessage(){
+   for(let i = 0; i < errorNodes.length; i++){
+      errorNodes[i].innerText = "";
+   }
+   success.innerTet = "";
+   nameInput.classList.remove("error-border");
+   email.classList.remove("error-border");
+   message.classList.remove("error-border");
+}
+
+//check if email is valid
+function emailIsValid(email){
+   let pattern = /\S+@\S+\.\S+/;
+   return pattern.test(email);
+}
